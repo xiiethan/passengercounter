1c56a18262ea514735f96fb6b18aac382c2f7dba
xiiethan <ethan_dillinger458@yahoo.com>
2023-02-02 18:40:00 +0000

    js updates

---
+++ index.js
@@ -0,0 +1,17 @@
+//let is to create a variable with block scope (private basically)
+//cannot redeclare let variables
+let count = 0;
+//console.log() literally just logs thing to the console, whatever you put in the ()
+console.log(count)
+
+//store the count and initalize as 0
+//listen for clicks on button
+//increment the count variable when clicked
+// change the count-el in HTML to reflect new count
+
+function increment(){
+    console.log("Button pushed")
+}
+
+
+//python -m http.server 8000
